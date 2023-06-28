560a871c42663e435bdc361062a156f285c2f436
dubleks <dubodelov.aleksey@gmail.com>
2023-06-28 13:52:58 +0200

    зробив навігацію через цсс. Плавні ховери

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 //-------------- scroool
 
-// const menuLinks = document.querySelectorAll('.linnk_item[data-goto]');
-// if (menuLinks.length > 0) {
-//     menuLinks.forEach(menuLink => {
-//         menuLink.addEventListener("click", onMenuLinkClick);
-//     });
-//     function onMenuLinkClick(e) {
-//         const menuLink = e.target;
-//         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-//             const gotoSection = document.querySelector(menuLink.dataset.goto);
-//             const gotoSectionValue = gotoSection.getBoundingClientRect().top + scrollY - document.querySelector('.navbar-scroll').offsetHeight; 
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('nav').offsetHeight; 
+            
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                menuBody.classList.remove('_active');
+                iconMenu.classList.remove('_active');
+                menuList.classList.remove('_active');
 
-//             window.scrollTo({
-//                 top: gotoSectionValue,
-//                 behavior: "smooth"
-//             });
-//             e.preventDefault();
-//         }
-//     }
-// }
+            }
+            
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
 
 //-------------- burger
+
+
 const iconMenu = document.querySelector('.btn-s');
 const menuBody = document.querySelector('.menu__body')
 const menuList = document.querySelector('.menu')
